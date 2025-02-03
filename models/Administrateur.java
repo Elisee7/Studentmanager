0a2685183765b3fba452a09cf4891f6a2bfe3965
unknown <175645022+Elisee7@users.noreply.github.com>
2025-02-03 17:53:28 +0000

    Version 5

--- models/Administrateur.java
+++ models/Administrateur.java
@@ -50,6 +50,12 @@ public class Administrateur extends Personne {
         double note = scan.nextDouble();
         return note;
     }
+    // Fonction pour initialiser le niveau de la classe
+    public String getNiveauClasse(Scanner scan) {
+        System.out.println("Le niveau de la classe est: ");
+        String niveau = scan.nextLine();
+        return niveau;
+    }
     
 
     // Fonction relatif à la classe Etudiant
@@ -59,7 +65,7 @@ public class Administrateur extends Personne {
      * @param classe
      * @param scan
      */
-    void creeEtudiant(Classe classe, Scanner scan) {
+    public void creeEtudiant(Classe classe, Scanner scan) {
         String nom = getNomPersonne(scan);
         int age = getAgePersonne(scan);
         //double moyenne = scan.nextDouble();
@@ -74,10 +80,11 @@ public class Administrateur extends Personne {
     }
 
     // Ajouter une note à un étudiant
-    void ajouterNoteEtudiant(Scanner scan, Classe classe) {
+    public void ajouterNoteEtudiant(Scanner scan, Classe classe) {
         String nom = getNomPersonne(scan);
         String module = getModule(scan);
         double note = getNoteModule(scan);
+        scan.nextLine();
         Map<String,Double> put = new HashMap<String,Double>();
         put.put(module, note);
         for (Etudiant etudiant : classe.etudiants) {
@@ -98,7 +105,7 @@ public class Administrateur extends Personne {
     }
 
     //Partie suppression de l'étudiant
-    void supprimerEtudiant(Classe classe, Scanner scan) {
+    public void supprimerEtudiant(Classe classe, Scanner scan) {
         String nom = getNomPersonne(scan);
         for (Etudiant etudiant : classe.etudiants) {
             if (etudiant.nom.equals(nom)) {
@@ -109,19 +116,25 @@ public class Administrateur extends Personne {
     }
     // Partie affichage des informations de l'étudiant
 
-    void afficherNotes(Etudiant etudiant) {
+    public void afficherNotes(Etudiant etudiant) {
         System.out.println(etudiant.nom + " : " + etudiant.getNote());
     }
 
-    void afficherInfos(Etudiant etudiant) {
+    public void afficherEtudiant(Etudiant etudiant) {
         etudiant.afficherInfos();
     }
 
-    void afficherMoyenne(Etudiant etudiant) {
+    public void afficherEtudiants(ArrayList<Etudiant> etudiants) {
+        for (Etudiant etudiant : etudiants) {
+            afficherEtudiant(etudiant);
+        }
+    }
+
+    public void afficherMoyenne(Etudiant etudiant) {
         System.out.println(etudiant.nom + " : " + etudiant.getMoyenne());
     }
     // Partie modification des informations de l'étudiant
-    void modifierNomEtudiant(Classe classe, Scanner scan) {
+    public void modifierNomEtudiant(Classe classe, Scanner scan) {
         String nom = getNomPersonne(scan);
         System.out.println("Le nouveau nom est: ");
         String newNom = scan.nextLine();
@@ -132,10 +145,11 @@ public class Administrateur extends Personne {
             }
         }
     }
-    void modifierAgeEtudiant(Classe classe, Scanner scan) {
+    public void modifierAgeEtudiant(Classe classe, Scanner scan) {
         String nom = getNomPersonne(scan);
         System.out.println("Le nouvel age est: ");
         int newAge = scan.nextInt();
+        scan.nextLine();
         for (Etudiant etudiant : classe.etudiants) {
             if (etudiant.nom.equals(nom)) {
                 etudiant.setAge(newAge);
@@ -143,37 +157,179 @@ public class Administrateur extends Personne {
             }
         }
     }
+    // Partie ajout d'etuudiant
+    public void ajouterEtudiant(Etudiant etudiant, ArrayList<Etudiant> etudiants) {
+        etudiants.add(etudiant);
+    }
 
     // Partie modification des notes de l'étudiant
-    void modifierNoteModule(Etudiant etudiant, Scanner scan) {
+    public void modifierNoteModule(Classe classe, Scanner scan) {
+        String nomEtudiant = getNomPersonne(scan);
         String module = getModule(scan);
         double note = getNoteModule(scan);
         boolean moduleFound = false;
-        for (Map<String, Double> noteMap : etudiant.getNote()) {
-            if (noteMap.containsKey(module)) {
-                noteMap.put(module, note);
-                moduleFound = true;
+
+        for (Etudiant etudiant : classe.etudiants) {
+            if (etudiant.getNom().equals(nomEtudiant)) {
+                for (Map<String, Double> noteMap : etudiant.getNote()) {
+                    if (noteMap.containsKey(module)) {
+                        noteMap.put(module, note);
+                        moduleFound = true;
+                        break;
+                    }
+                }
+                if (!moduleFound) {
+                    Map<String, Double> newNoteMap = new HashMap<>();
+                    newNoteMap.put(module, note);
+                    etudiant.getNote().add(newNoteMap);
+                }
+                // Calcul de la moyenne
+                double somme = 0;
+                for (Map<String, Double> n : etudiant.getNote()) {
+                    for (Map.Entry<String, Double> entry : n.entrySet()) {
+                        somme += entry.getValue();
+                    }
+                }
+                etudiant.setMoyenne(somme / etudiant.getNote().size());
                 break;
             }
         }
-        if (!moduleFound) {
-            Map<String, Double> newNoteMap = new HashMap<>();
-            newNoteMap.put(module, note);
-            etudiant.getNote().add(newNoteMap);
-        }
     }
-    void modifierNom(Etudiant etudiant, String nom) {
-        etudiant.setNom(nom);
-    }
-    void modifierAge(Etudiant etudiant, int age) {
-        etudiant.setAge(age);
-    }
-
-
     
     // Fonction relatif à la classe Professeur
 
+    // Creations d'un professeur
+    public void creerProfesseur(Classe classe, Scanner scan) {
+        String nom = getNomPersonne(scan);
+        int age = getAgePersonne(scan);
+        ArrayList<String> moduleEnseigner = new ArrayList<>();
+        System.out.println("Entrez le nombre de modules à enseigner: ");
+        int nbModules = scan.nextInt();
+        scan.nextLine();
+        for (int i = 0; i < nbModules; i++) {
+            System.out.println("Entrez le module à enseigner: ");
+            String module = scan.nextLine();
+            moduleEnseigner.add(module);
+        }
+        Professeur professeur = new Professeur(nom, age, moduleEnseigner);
+        classe.professeurs.add(professeur);
+    }
+
+    // Suppression d'un professeur
+    public void supprimerProfesseur(Classe classe, Scanner scan) {
+        String nom = getNomPersonne(scan);
+        for (Professeur professeur : classe.professeurs) {
+            if (professeur.nom.equals(nom)) {
+                classe.professeurs.remove(professeur);
+                break;
+            }
+        }
+    }
+
+    // Affichage des informations d'un professeur
+    public void afficherInfos(Professeur professeur) {
+        System.out.println("Professeur : " + professeur.nom + ", Âge : " + professeur.age + ", Modules enseignés : " + professeur.getModuleEnseigner());
+    }
+
+    // Modification des informations d'un professeur
+    public void modifierNomProfesseur(Classe classe, Scanner scan) {
+        String nom = getNomPersonne(scan);
+        System.out.println("Le nouveau nom est: ");
+        String newNom = scan.nextLine();
+        for (Professeur professeur : classe.professeurs) {
+            if (professeur.nom.equals(nom)) {
+                professeur.setNom(newNom);
+                break;
+            }
+        }
+    }
+
+    // Modification du d'un professeur
+    public void modifierAgeProfesseur(Classe classe, Scanner scan) {
+        String nom = getNomPersonne(scan);
+        System.out.println("Le nouvel age est: ");
+        int newAge = scan.nextInt();
+        for (Professeur professeur : classe.professeurs) {
+            if (professeur.nom.equals(nom)) {
+                professeur.setAge(newAge);
+                break;
+            }
+        }
+    }
+
+    // Ajout d'un module à enseigner
+    public void ajouterModuleEnseigner(Classe classe, Scanner scan) {
+        String nom = getNomPersonne(scan);
+        System.out.println("Entrez le module à enseigner: ");
+        String module = scan.nextLine();
+        for (Professeur professeur : classe.professeurs) {
+            if (professeur.nom.equals(nom)) {
+                professeur.getModuleEnseigner().add(module);
+                break;
+            }
+        }
+    }
+
+    // Suppression d'un module à enseigner
+    public void supprimerModuleEnseigner(Classe classe, Scanner scan) {
+        String nom = getNomPersonne(scan);
+        System.out.println("Entrez le module à supprimer: ");
+        String module = scan.nextLine();
+        for (Professeur professeur : classe.professeurs) {
+            if (professeur.nom.equals(nom)) {
+                professeur.getModuleEnseigner().remove(module);
+                break;
+            }
+        }
+    }
+
+
     // Fonction relatif à la classe Classe
 
+    // Création d'une classe
+    public void creerClasse(Scanner scan) {
+        String niveau = getNiveauClasse(scan);
+        Classe classe = new Classe(niveau, new ArrayList<Professeur>(), new ArrayList<Etudiant>());
+        listeClasses.add(classe);
+    }
+
+    // Suppression d'une classe
+    public void supprimerClasse(Scanner scan) {
+        System.out.println("Entrez le niveau de la classe à supprimer: ");
+        String niveau = scan.nextLine();
+        for (Classe classe : listeClasses) {
+            if (classe.niveau.equals(niveau)) {
+                listeClasses.remove(classe);
+                break;
+            }
+        }
+    }
+
+    // Affichage des informations d'une classe
+    public void afficherInfos(Classe classe) {
+        System.out.println("Classe : " + classe.niveau);
+        System.out.println("Professeurs : ");
+        for (Professeur professeur : classe.professeurs) {
+            System.out.println(professeur.nom + " : " + professeur.getModuleEnseigner());
+        }
+        System.out.println("Étudiants : ");
+        for (Etudiant etudiant : classe.etudiants) {
+            System.out.println(etudiant.nom + " : " + etudiant.getMoyenne());
+        }
+    }
+    // Modification du niveau de la classe
+    public void modifierNiveauClasse(Scanner scan) {
+        System.out.println("Entrez le niveau de la classe à modifier: ");
+        String niveau = scan.nextLine();
+        System.out.println("Entrez le nouveau niveau: ");
+        String newNiveau = scan.nextLine();
+        for (Classe classe : listeClasses) {
+            if (classe.niveau.equals(niveau)) {
+                classe.setNiveau(newNiveau);
+                break;
+            }
+        }
+    }
+    // Ajout d'un professeur à une classe
 
 }
--- ui/Main.java
+++ ui/Main.java
@@ -1,8 +1,57 @@
 package ui;
 
+import models.Administrateur;
+import models.Classe;
+import models.Etudiant;
+import models.Professeur;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+
+        // Créer une classe
+        ArrayList<Professeur> professeurs = new ArrayList<>();
+        ArrayList<Etudiant> etudiants = new ArrayList<>();
+        Classe classe = new Classe("Terminale", professeurs, etudiants);
+
+        // Créer un administrateur
+        ArrayList<Classe> listeClasses = new ArrayList<>();
+        listeClasses.add(classe);
+        Administrateur admin = new Administrateur("Admin", 45, listeClasses);
+
+        // Ajouter un professeur
+        System.out.println("Ajout d'un professeur :");
+        admin.creerProfesseur(classe, scan);
+
+        // Ajouter un étudiant
+        System.out.println("Ajout d'un étudiant :");
+        admin.ajouterEtudiant(new Etudiant("Etudiant1", 18, classe), etudiants);
+
+        // Modifier l'âge d'un étudiant
+        System.out.println("Modification de l'âge d'un étudiant :");
+        admin.modifierAgeEtudiant(classe, scan);
+
+        // Ajouter une note à un étudiant
+        System.out.println("Ajout d'une note à un étudiant :");
+        admin.ajouterNoteEtudiant(scan, classe);
+
+        // Modifier une note d'un étudiant
+        System.out.println("Modification d'une note d'un étudiant :");
+        admin.modifierNoteModule(classe, scan);
+
+        // Afficher les informations des étudiants
+        System.out.println("Affichage des informations des étudiants :");
+        admin.afficherEtudiants(etudiants);
+
+        // Afficher les informations des professeurs
+        System.out.println("Affichage des informations des professeurs :");
+        for (Professeur professeur : classe.professeurs) {
+            admin.afficherInfos(professeur);
+        }
+
+        scan.close();
     }
-    
 }
